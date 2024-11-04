39bd4b43c035b95f79a60b3a959beed07ad15a24
츺늘 <114144989+cfnrgystwjd@users.noreply.github.com>
2024-11-04 15:30:57 +0900

    Add files via upload

---
+++ pkcs.skeleton.c
@@ -0,0 +1,166 @@
+/*
+ * Copyright(c) 2020-2024 All rights reserved by Heekuck Oh.
+ * 이 프로그램은 한양대학교 ERICA 컴퓨터학부 학생을 위한 교육용으로 제작되었다.
+ * 한양대학교 ERICA 학생이 아닌 자는 이 프로그램을 수정하거나 배포할 수 없다.
+ * 프로그램을 수정할 경우 날짜, 학과, 학번, 이름, 수정 내용을 기록한다.
+ */
+#ifdef __linux__
+#include <bsd/stdlib.h>
+#elif __APPLE__
+#include <stdlib.h>
+#else
+#include <stdlib.h>
+#endif
+#include <string.h>
+#include <gmp.h>
+#include "pkcs.h"
+#include "sha2.h"
+
+/*
+ * rsa_generate_key() - generates RSA keys e, d and n in octet strings.
+ * If mode = 0, then e = 65537 is used. Otherwise e will be randomly selected.
+ * Carmichael's totient function Lambda(n) is used.
+ */
+void rsa_generate_key(void *_e, void *_d, void *_n, int mode)
+{
+    mpz_t p, q, p_1, q_1, lambda, e, d, n, gcd;
+    gmp_randstate_t state;
+    
+    /*
+     * Initialize mpz variables
+     */
+    mpz_inits(p, q, p_1, q_1, lambda, e, d, n, gcd, NULL);
+    gmp_randinit_default(state);
+    gmp_randseed_ui(state, arc4random());
+    /*
+     * Generate prime p and q such that 2^(RSAKEYSIZE-1) <= p*q < 2^RSAKEYSIZE
+     * (p-1) and (q-1) are relatively prime to 2^16+1 (65537).
+     */
+    do {
+        /*
+         * Select a random prime p, where (p-1) is relatively prime to 65537.
+         */
+        do {
+            do {
+                mpz_urandomb(p, state, RSAKEYSIZE/2);
+                mpz_setbit(p, 0);
+                mpz_setbit(p, RSAKEYSIZE/2-1);
+            } while (mpz_probab_prime_p(p, 50) == 0);
+            mpz_sub_ui(p_1, p, 1);
+        } while (mpz_gcd_ui(gcd, p_1, 65537) != 1);
+        /*
+         * Select a random prime q, where (q-1) is relatively prime to 65537.
+         */
+        do {
+            do {
+                mpz_urandomb(q, state, RSAKEYSIZE/2);
+                mpz_setbit(q, 0);
+                mpz_setbit(q, RSAKEYSIZE/2-1);
+            } while (mpz_probab_prime_p(q, 50) == 0);
+            mpz_sub_ui(q_1, q, 1);
+        } while (mpz_gcd_ui(gcd, q_1, 65537) != 1);
+        /*
+         * Compute n = p * q
+         */
+        mpz_mul(n, p, q);
+    } while (!mpz_tstbit(n, RSAKEYSIZE-1));
+    /*
+     * Generate e and d using Lambda(n)
+     */
+    mpz_lcm(lambda, p_1, q_1);
+    if (mode == 0)
+        mpz_set_ui(e, 65537);
+    else do {
+        mpz_urandomb(e, state, RSAKEYSIZE);
+        mpz_gcd(gcd, e, lambda);
+    } while (mpz_cmp(e, lambda) >= 0 || mpz_cmp_ui(gcd, 1) != 0);
+    mpz_invert(d, e, lambda);
+    /*
+     * Convert mpz_t values into octet strings
+     */
+    mpz_export(_e, NULL, 1, RSAKEYSIZE/8, 1, 0, e);
+    mpz_export(_d, NULL, 1, RSAKEYSIZE/8, 1, 0, d);
+    mpz_export(_n, NULL, 1, RSAKEYSIZE/8, 1, 0, n);
+    /*
+     * Free the space occupied by mpz variables
+     */
+    mpz_clears(p, q, p_1, q_1, lambda, e, d, n, gcd, NULL);
+}
+}
+
+/*
+ * rsa_cipher() - compute m^k mod n
+ * If m >= n then returns PKCS_MSG_OUT_OF_RANGE, otherwise returns 0 for success.
+ */
+static int rsa_cipher(void *_m, const void *_k, const void *_n)
+{
+    mpz_t m, k, n;
+    
+    /*
+     * Initialize mpz variables
+     */
+    mpz_inits(m, k, n, NULL);
+    /*
+     * Convert big-endian octets into mpz_t values
+     */
+    mpz_import(m, RSAKEYSIZE/8, 1, 1, 1, 0, _m);
+    mpz_import(k, RSAKEYSIZE/8, 1, 1, 1, 0, _k);
+    mpz_import(n, RSAKEYSIZE/8, 1, 1, 1, 0, _n);
+    /*
+     * Compute m^k mod n
+     */
+    if (mpz_cmp(m, n) >= 0) {
+        mpz_clears(m, k, n, NULL);
+        return PKCS_MSG_OUT_OF_RANGE;
+    }
+    mpz_powm(m, m, k, n);
+    /*
+     * Convert mpz_t m into the octet string _m
+     */
+    mpz_export(_m, NULL, 1, RSAKEYSIZE/8, 1, 0, m);
+    /*
+     * Free the space occupied by mpz variables
+     */
+    mpz_clears(m, k, n, NULL);
+    return 0;
+}
+
+/*
+ * rsaes_oaep_encrypt() - RSA encrytion with the EME-OAEP encoding method
+ * 길이가 len 바이트인 메시지 m을 공개키 (e,n)으로 암호화한 결과를 c에 저장한다.
+ * label은 데이터를 식별하기 위한 라벨 문자열로 NULL을 입력하여 생략할 수 있다.
+ * sha2_ndx는 사용할 SHA-2 해시함수 색인 값으로 SHA224, SHA256, SHA384, SHA512,
+ * SHA512_224, SHA512_256 중에서 선택한다. c의 크기는 RSAKEYSIZE와 같아야 한다.
+ * 성공하면 0, 그렇지 않으면 오류 코드를 넘겨준다.
+ */
+int rsaes_oaep_encrypt(const void *m, size_t mLen, const void *label, const void *e, const void *n, void *c, int sha2_ndx)
+{
+}
+
+/*
+ * rsaes_oaep_decrypt() - RSA decrytion with the EME-OAEP encoding method
+ * 암호문 c를 개인키 (d,n)을 사용하여 원본 메시지 m과 길이 len을 회복한다.
+ * label과 sha2_ndx는 암호화할 때 사용한 것과 일치해야 한다.
+ * 성공하면 0, 그렇지 않으면 오류 코드를 넘겨준다.
+ */
+int rsaes_oaep_decrypt(void *m, size_t *mLen, const void *label, const void *d, const void *n, const void *c, int sha2_ndx)
+{
+}
+
+/*
+ * rsassa_pss_sign - RSA Signature Scheme with Appendix
+ * 길이가 len 바이트인 메시지 m을 개인키 (d,n)으로 서명한 결과를 s에 저장한다.
+ * s의 크기는 RSAKEYSIZE와 같아야 한다. 성공하면 0, 그렇지 않으면 오류 코드를 넘겨준다.
+ */
+int rsassa_pss_sign(const void *m, size_t mLen, const void *d, const void *n, void *s, int sha2_ndx)
+{
+}
+
+/*
+ * rsassa_pss_verify - RSA Signature Scheme with Appendix
+ * 길이가 len 바이트인 메시지 m에 대한 서명이 s가 맞는지 공개키 (e,n)으로 검증한다.
+ * 성공하면 0, 그렇지 않으면 오류 코드를 넘겨준다.
+ */
+int rsassa_pss_verify(const void *m, size_t mLen, const void *e, const void *n, const void *s, int sha2_ndx)
+{
+}
